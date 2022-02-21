7de7f12d98846fc04c9f61f0b2832fabb2c10f6d
Michael Buffone <m_buffone@hotmail.com>
2022-02-21 16:44:23 -0500

    added conn.on(data, (data) => {     console.log(data);   })

--- play.js
+++ play.js
@@ -3,14 +3,14 @@ const net = require('net');
 //establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '10.37.229.92',
+    host: '192.168.2.45',
     port: 50541
   });
 
   //interpret incoming data as text
   conn.setEncoding('utf8');
-  conn.on('connect', () => {
-    console.log('make a move');
+  conn.on('data', (data) => {
+    console.log(data);
   })
 
   return conn;
