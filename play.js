af1e6e4ed557eaf7add66a0845d0d39de3118292
Michael Buffone <m_buffone@hotmail.com>
2022-02-23 15:34:39 -0500

    added some comments to my code

--- play.js
+++ play.js
@@ -1,9 +1,15 @@
+// uses the connect function from client.js
 const { connect } = require('./client');
+
+// uses the setInput function from input.js
 const { setupInput } = require('./input');
 
+//shows the the server is connecting
 console.log('Connecting...');
 
+// create a variable conn for connect() to put in setInput();
 const conn = connect();
 
+//connects the setInput function from input.js to connect();
 setupInput(conn);
 
